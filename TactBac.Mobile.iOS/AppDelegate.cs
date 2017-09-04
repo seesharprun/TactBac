ad8819d8d380d9f1c7507987e94fe8f818d86a43
Sidney Andrews <sidney@seesharprun.net>
2017-09-03 22:27:59 -0400

    Updated iOS project

--- TactBac.Mobile.iOS/AppDelegate.cs
+++ TactBac.Mobile.iOS/AppDelegate.cs
@@ -1,31 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-using Foundation;
-using UIKit;
-using Prism.Unity;
+﻿using Foundation;
 using Microsoft.Practices.Unity;
+using Prism.Events;
+using Prism.Unity;
+using TactBac.Mobile.Events;
+using TactBac.Mobile.iOS.Services;
+using TactBac.Mobile.Services;
+using UIKit;
 
 namespace TactBac.Mobile.iOS
 {
-    // The UIApplicationDelegate for the application. This class is responsible for launching the 
-    // User Interface of the application, as well as listening (and optionally responding) to 
-    // application events from iOS.
     [Register("AppDelegate")]
     public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
     {
-        //
-        // This method is invoked when the application has loaded and is ready to run. In this 
-        // method you should instantiate the window, load the UI into it and then make the window
-        // visible.
-        //
-        // You have 17 seconds to return from this method, or iOS will terminate your application.
-        //
+        private IEventAggregator _eventAggregator;
+
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
-            LoadApplication(new App(new iOSInitializer()));
+
+            var application = new App(new iOSInitializer());
+
+            var container = application.Container;
+            _eventAggregator = container.Resolve<IEventAggregator>();
+            _eventAggregator.GetEvent<StartButtonStatus>().Publish(true);
+
+            LoadApplication(application);
 
             return base.FinishedLaunching(app, options);
         }
@@ -35,8 +34,7 @@ namespace TactBac.Mobile.iOS
     {
         public void RegisterTypes(IUnityContainer container)
         {
-
+            container.RegisterType<IContactService, AppleContactService>();
         }
     }
-
 }
---
+++ TactBac.Mobile.iOS/Services/AppleContactService.cs
@@ -0,0 +1,42 @@
+﻿using Contacts;
+using Foundation;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TactBac.Mobile.Models;
+using TactBac.Mobile.Services;
+
+namespace TactBac.Mobile.iOS.Services
+{
+    public class AppleContactService : IContactService
+    {
+        public async Task<IEnumerable<Contact>> GetContactNamesAsync()
+        {
+            return await Task<IEnumerable<Contact>>.Run(() =>
+            {
+                List<Contact> contacts = new List<Contact>();
+                NSError error;
+                CNContact[] contactList;
+                var keysTOFetch = new[] { CNContactKey.GivenName, CNContactKey.FamilyName, CNContactKey.EmailAddresses };
+                var ContainerId = new CNContactStore().DefaultContainerIdentifier;
+                using (var predicate = CNContact.GetPredicateForContactsInContainer(ContainerId))
+                using (var store = new CNContactStore())
+                {
+                    contactList = store.GetUnifiedContacts(predicate, keysTOFetch, out error);
+                }
+                foreach (var item in contactList)
+                {
+                    if (null != item && null != item.EmailAddresses)
+                    {
+                        contacts.Add(new Contact
+                        {
+                            DisplayName = $"{item.GivenName} {item.FamilyName}",
+                            EmailAddress = item.EmailAddresses.FirstOrDefault()?.Value
+                        });
+                    }
+                }
+                return contacts;
+            });
+        }
+    }
+}
